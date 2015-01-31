3515bc6bf1620abe137c6f3ddd129ad55945f3ac
huyingying <1255793815@qq.com>
2015-01-31 19:48:55 +0800

    huyingying20150131

---
+++ JBLBrowser/src/com/jbl/browser/activity/ToolBarOperateActivity.java
@@ -0,0 +1,5 @@
+package com.jbl.browser.activity;
+
+public class ToolBarOperateActivity extends BaseFragActivity{
+	
+}
---
+++ JBLBrowser/src/com/jbl/browser/fragment/BookMarkFragment.java
@@ -0,0 +1,44 @@
+package com.jbl.browser.fragment;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.actionbarsherlock.app.ActionBar;
+import com.actionbarsherlock.app.SherlockFragment;
+import com.actionbarsherlock.view.Menu;
+import com.actionbarsherlock.view.MenuInflater;
+import com.jbl.browser.R;
+
+public class BookMarkFragment extends SherlockFragment{
+	public final static String TAG="BookMarkFragment";
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		// TODO Auto-generated method stub
+		super.onCreate(savedInstanceState);
+	}
+	@Override
+	public void onActivityCreated(Bundle savedInstanceState) {
+		// TODO Auto-generated method stub
+		super.onActivityCreated(savedInstanceState);
+		final ActionBar ab = this.getSherlockActivity().getSupportActionBar();
+
+		// set defaults for logo & home up
+		ab.setDisplayHomeAsUpEnabled(false);
+		ab.setDisplayUseLogoEnabled(false);
+		ab.setDisplayShowHomeEnabled(false);
+		setHasOptionsMenu(true);
+	}
+	@Override
+	public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
+		// TODO Auto-generated method stub
+		super.onCreateOptionsMenu(menu, inflater);
+	}
+	@Override
+	public View onCreateView(LayoutInflater inflater, ViewGroup container,
+			Bundle savedInstanceState) {
+		View view = inflater.inflate(R.layout.bookmark_fragment, container, false);
+		return super.onCreateView(inflater, container, savedInstanceState);
+	}
+}
