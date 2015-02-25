0c12a663190d0c0a197b57bdcd7f416333c8ba56
liumingran <1209725754@qq.com>
2015-02-25 15:56:13 +0800

    liumingran20150224

--- JBLBrowser/src/com/jbl/browser/adapter/SettingPagerAdapter.java
+++ JBLBrowser/src/com/jbl/browser/adapter/SettingPagerAdapter.java
@@ -1,12 +1,14 @@
 package com.jbl.browser.adapter;
 
 import java.util.List;
+import java.util.Map;
 
+import android.content.Context;
 import android.os.Parcelable;
 import android.support.v4.view.PagerAdapter;
 import android.support.v4.view.ViewPager;
 import android.view.View;
-
+import android.widget.GridView;
 import com.viewpager.indicator.IconPagerAdapter;
 
 /**
@@ -15,53 +17,36 @@ import com.viewpager.indicator.IconPagerAdapter;
  *
  */
 public class SettingPagerAdapter extends PagerAdapter  implements IconPagerAdapter
-{  
-    private static final String TAG = "SettingPagerAdapter";  
-    private List<View> mViewPages;  
-  
-    public SettingPagerAdapter(List<View> viewPages) {  
-        mViewPages = viewPages;  
-    }  
-    @Override  
-    public int getCount()  
-    {  
-        // TODO Auto-generated method stub  
-        return mViewPages.size();  
-    }  
-    @Override  
-    public boolean isViewFromObject(View arg0, Object arg1)  
-    {  
-        // TODO Auto-generated method stub  
-        return arg0 == arg1;  
-    }  
-    @Override  
-    public void destroyItem(View v, int position, Object object)  
-    {  
-        // TODO Auto-generated method stub  
-        ((ViewPager) v).removeView(mViewPages.get(position));  
-    }  
-  
-    @Override  
-    public void finishUpdate(View container)  
-    {  
-        // TODO Auto-generated method stub  
-    }  
-  
-    @Override  
-    public int getItemPosition(Object object)  
-    {  
-        // TODO Auto-generated method stub  
-        return super.getItemPosition(object);  
-    }  
-  
-    @Override  
-    public Object instantiateItem(View v, int position)  
-    {  
-        // TODO Auto-generated method stub  
-        ((ViewPager) v).addView(mViewPages.get(position));  
-        return mViewPages.get(position);  
-    }  
-  
+{  private Map<Integer, GridView>  map;
+    public static  List<View> mViewPages;  
+    public SettingPagerAdapter(Context context, List<GridView> array) {
+   }
+   public SettingPagerAdapter(Context context,Map<Integer, GridView> map){
+	this.map=map;
+  }
+  @Override
+  public int getCount() {
+	// TODO Auto-generated method stub
+	return map.size();
+  }
+   @Override
+  public boolean isViewFromObject(View arg0, Object arg1) {
+	// TODO Auto-generated method stub
+	return arg0 == arg1;
+ }
+
+  @Override
+  public Object instantiateItem(View arg0, int arg1) {
+	((ViewPager) arg0).addView(map.get(arg1));
+
+	return map.get(arg1);
+ }
+
+  @Override
+  public void destroyItem(View arg0, int arg1, Object arg2) {
+	((ViewPager) arg0).removeView((View) arg2);
+	//
+  }
     @Override  
     public void restoreState(Parcelable state, ClassLoader loader)  
     {  
@@ -86,7 +71,4 @@ public class SettingPagerAdapter extends PagerAdapter  implements IconPagerAdapt
 		// TODO Auto-generated method stub
 		return 0;
 	}  
-	
-	
-  
 }  
--- JBLBrowser/src/com/jbl/browser/fragment/SettingPagerFragment.java
+++ JBLBrowser/src/com/jbl/browser/fragment/SettingPagerFragment.java
@@ -1,11 +1,10 @@
 package com.jbl.browser.fragment;
 
 import java.util.ArrayList;
-import java.util.List;
-
-import android.content.Context;
+import java.util.HashMap;
+import java.util.Map;
+import android.content.Intent;
 import android.os.Bundle;
-import android.support.v4.view.PagerAdapter;
 import android.support.v4.view.ViewPager;
 import android.view.LayoutInflater;
 import android.view.MotionEvent;
@@ -16,14 +15,13 @@ import android.widget.AdapterView;
 import android.widget.AdapterView.OnItemClickListener;
 import android.widget.GridView;
 import android.widget.RelativeLayout;
-
 import com.actionbarsherlock.app.SherlockFragment;
 import com.jbl.browser.R;
 import com.jbl.browser.activity.BaseFragActivity;
 import com.jbl.browser.adapter.SettingGridItemAdapter;
 import com.jbl.browser.adapter.SettingPagerAdapter;
 import com.jbl.browser.interfaces.SettingItemInterface;
-import com.jbl.browser.utils.JBLPreference;
+import com.jbl.browser.utils.ImageInfo;
 import com.viewpager.indicator.LinePageIndicator;
 import com.viewpager.indicator.PageIndicator;
 
@@ -33,51 +31,42 @@ import com.viewpager.indicator.PageIndicator;
  */
 
 public class SettingPagerFragment extends SherlockFragment{
-	public static final String TAG = "SettingPagerFragment";
-	
-	private ViewPager mViewPager; // 水平实现滑动效果
-	private PagerAdapter mPageAdapter;	
-	PageIndicator mIndicator;
-	private static final int PAGE_SIZE = 8; // 每页显示的数据个数
-	private static final int TEST_LIST_SIZE = 43; // 数据总长度
-	int sTotalPages = 1;
-	private int mCurrentPage;
-	private List<List<String>> mPageList;
-	private List<GridView> mGridViews;
-	private Context mContext;
+	private static final float APP_PAGE_SIZE = 8.0f;
+	private SettingPagerAdapter adapter;
+	private ViewPager viewPager;
+	private ArrayList<ImageInfo> list;
 	private String[] resArrays;
-	private List<View> mViewPages;
-	private boolean flag=true;//true :开启无图  false:关闭无图
+	private Map<Integer, GridView> map;
+	public static final String TAG = "SettingPagerFragment";
+	private PageIndicator mIndicator;
+	private int PageCount;
 	private View mView;
 	private RelativeLayout mRelativeLayout;
+	//点击回调接口
+	private SettingItemInterface settingInterface;
 	/*
 	 * caidantubiao
 	 */
-	private int[] girdview_menu_image = {R.drawable.menu_add_bookmark_selector,R.drawable.menu_combine_selector,R.drawable.menu_setting_selector,
-			R.drawable.menu_nightmode_selector,R.drawable.menu_share_selector,R.drawable.no_pic_mode_selector,R.drawable.menu_download_selector,
-			R.drawable.menu_quit_selector,R.drawable.ic_launcher,R.drawable.menu_wuhen_selector,R.drawable.menu_fullscreen_selector,
-			R.drawable.menu_refresh_selector,R.drawable.menu_feedback_selector,R.drawable.ic_launcher};
-		
-	//点击回调接口
-	private SettingItemInterface settingInterface;
-	
+	private int[] girdview_menu_image = {R.drawable.menu_add_book_mark,R.drawable.menu_bookmark,R.drawable.menu_settings,
+			R.drawable.menu_bookmark,R.drawable.menu_share,R.drawable.menu_nopic,R.drawable.menu_download,
+			R.drawable.menu_exit,R.drawable.menu_scrollbutton,R.drawable.menu_nofoot,R.drawable.menu_fullscreen,
+			R.drawable.menu_refresh,R.drawable.menu_feedback,R.drawable.menu_nightmode};
 	@Override
 	public View onCreateView(LayoutInflater inflater, ViewGroup container,
 			Bundle savedInstanceState) {
-		initData();
-		
+		inflater = getLayoutInflater(savedInstanceState);
 		View view=inflater.inflate(R.layout.main_setting_panel, container, false);
-		
-		mRelativeLayout=(RelativeLayout)view.findViewById(R.id.panel_rl);
-		mViewPager = (ViewPager) view.findViewById(R.id.setting_viewpager);
+		initPages();
+		initViewAndAdapter();
+		viewPager = (ViewPager)view. findViewById(R.id.setting_viewpager);
 		mIndicator = (LinePageIndicator)view.findViewById(R.id.setting_indicator);
-		mPageAdapter = new SettingPagerAdapter(getPageViews());
-		mViewPager.setAdapter(mPageAdapter);
-		mIndicator.setViewPager(mViewPager);
-		mViewPager.startAnimation(AnimationUtils.loadAnimation(getActivity(),R.anim.menu_bar_appear));// 加载弹出菜单栏的动画效果
+		mRelativeLayout=(RelativeLayout)view.findViewById(R.id.panel_rl);
+		adapter = new SettingPagerAdapter(getActivity(), map);
+		viewPager.setAdapter(adapter);
+		mIndicator.setViewPager(viewPager);
+		viewPager.startAnimation(AnimationUtils.loadAnimation(getActivity(),R.anim.menu_bar_appear));// 加载弹出菜单栏的动画效果
 		mView=(View)view.findViewById(R.id.fill_pad);
 		mView.setOnTouchListener(new View.OnTouchListener() {
-			
 			@Override
 			public boolean onTouch(View v, MotionEvent event) {
 				// TODO Auto-generated method stub
@@ -100,172 +89,145 @@ public class SettingPagerFragment extends SherlockFragment{
 	
 	@Override
 	public void onStop() {
-		mViewPager.startAnimation(AnimationUtils.loadAnimation(getActivity(),R.anim.menu_bar_disappear));// 退出菜单栏时的动画效果
+		viewPager.startAnimation(AnimationUtils.loadAnimation(getActivity(),R.anim.menu_bar_disappear));// 退出菜单栏时的动画效果
 		super.onStop();
 	}
 	
-	/**
-	 * 初始化数据源
-	 */
-	void initData(){
-		mContext=this.getActivity();
-		resArrays= mContext.getResources().getStringArray(R.array.setting_content_item);		
-		mPageList = new ArrayList<List<String>>();
-		mGridViews = new ArrayList<GridView>();
-		mViewPages = new ArrayList<View>();
-		initPages(resArrays);
-		initViewAndAdapter();
-	}
+	
 	/**
 	 * 将数据分页
 	 * 
 	 * @param list
 	 */
-	public void initPages(String[] list) {
-		if (list.length % PAGE_SIZE == 0) {
-			sTotalPages = list.length / PAGE_SIZE;
-		} else {
-			sTotalPages =list.length/ PAGE_SIZE + 1;
-		}
-		mCurrentPage = 0;
-		List<String> l = new ArrayList<String>();
-		for (int i = 0; i < list.length; ++i) {
-			if(i==5){
-				if(JBLPreference.getInstance(mContext).readInt(JBLPreference.PIC_CACHE_TYPE)==0){
-					l.add(list[i].substring(4));
-				}else{
-					l.add(list[i].substring(0,4));
-				}
-			}else if(i==8){
-				   if(JBLPreference.getInstance(mContext).readInt(JBLPreference.TURNING_TYPE)==0){
-					   l.add(list[i].substring(4));
-				   }else{
-					   l.add(list[i].substring(0,4));
-				   }
-			}else if(i==9){
-					if(JBLPreference.getInstance(mContext).readInt(JBLPreference.HISTORY_CACHE_TYPE)==0){
+	public void initPages() {
+		final Intent mainIntent = new Intent(Intent.ACTION_MAIN, null);
+		mainIntent.addCategory(Intent.CATEGORY_LAUNCHER);
+		 list = new ArrayList<ImageInfo>();
+	        resArrays=getResources().getStringArray(R.array.setting_content_item);	
+	        for(int i=0;i<resArrays.length;i++){
+	            list.add(new ImageInfo(girdview_menu_image[i] , resArrays[i]));
+	           /* List<String> l = new ArrayList<String>();
+	            if(i==5){
+					if(JBLPreference.getInstance(mContext).readInt(JBLPreference.PIC_CACHE_TYPE)==0){
 						l.add(list[i].substring(4));
 					}else{
 						l.add(list[i].substring(0,4));
-				    }
-				 }else if(i==10){
-					 if(JBLPreference.getInstance(mContext).readInt(JBLPreference.FULL_SCREEN_TYPE)==0){
+					}
+				}else if(i==8){
+					   if(JBLPreference.getInstance(mContext).readInt(JBLPreference.TURNING_TYPE)==0){
+						   l.add(list[i].substring(4));
+					   }else{
+						   l.add(list[i].substring(0,4));
+					   }
+				}else if(i==9){
+						if(JBLPreference.getInstance(mContext).readInt(JBLPreference.HISTORY_CACHE_TYPE)==0){
 							l.add(list[i].substring(4));
 						}else{
 							l.add(list[i].substring(0,4));
 					    }
-				}else{
-						l.add(list[i]);
-					}
-			if ((i + 1) % PAGE_SIZE == 0) {
-				mPageList.add(l);
-				l = new ArrayList<String>();
-			}
-		}
-		if (l.size() > 0) {
-			mPageList.add(l);
-		}
-		
+					 }else if(i==10){
+						 if(JBLPreference.getInstance(mContext).readInt(JBLPreference.FULL_SCREEN_TYPE)==0){
+								l.add(list[i].substring(4));
+							}else{
+								l.add(list[i].substring(0,4));
+						    }
+					}else{
+							l.add(list[i]);
+						}*/
+	        }
 	}
-
-	
 	public void setInterface(SettingItemInterface i){
 		settingInterface=i;
 	}
-	
 	private void initViewAndAdapter() {
-		LayoutInflater inflater = (LayoutInflater) mContext
-				.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
-		for (int i = 0; i < sTotalPages; ++i) {
-			GridView lv = (GridView)inflater.inflate(R.layout.main_setting_gridview, null);
-			mGridViews.add(lv);
-			SettingGridItemAdapter adapter = new SettingGridItemAdapter(mContext,mPageList.get(i));
-			lv.setAdapter(adapter);
-			mViewPages.add(lv);
-			if (i == 0) {
-				// 菜单监听事件
-				lv.setOnItemClickListener(new OnItemClickListener() {
-					@Override
-					public void onItemClick(AdapterView<?> parent,
-							View view, int position, long id) {
-						// TODO Auto-generated method stub
-						switch (position) {
-						case 0: // 添加书签	
-							if(settingInterface!=null)
-								settingInterface.addBookMark();
-							break;
-						case 1: // 跳转到书签界面
-							if(settingInterface!=null)
-								settingInterface.listBookMark();
-							break;
+		 PageCount = (int) Math.ceil(list.size() / APP_PAGE_SIZE);
+			map = new HashMap<Integer, GridView>();
+			for (int i = 0; i < PageCount; i++) {
+				GridView appPage = new GridView(getActivity());
+				final SettingGridItemAdapter adapter =new SettingGridItemAdapter(getActivity(), list, i);
+				appPage.setAdapter(adapter);
+				appPage.setNumColumns(4);
+				map.put(i, appPage);
+				if (i == 0) {
+					// 菜单监听事件
+					appPage.setOnItemClickListener(new OnItemClickListener() {
+						@Override
+						public void onItemClick(AdapterView<?> parent,
+								View view, int position, long id) {
+							// TODO Auto-generated method stub
+							switch (position) {
+							case 0: // 添加书签	
+								if(settingInterface!=null)
+									settingInterface.addBookMark();
+								break;
+							case 1: // 跳转到书签界面
+								if(settingInterface!=null)
+									settingInterface.listBookMark();
+								break;
+							case 2://跳转到设置界面
+								if(settingInterface!=null)
+									settingInterface.browserSetting();
+								break;
+							case 3: // 跳转到历史记录界面
+								if(settingInterface!=null)
+									settingInterface.listHistory();
+								break;
+							case 4://分享
+								if(settingInterface!=null)
+									settingInterface.share();
+								break;
+							case 5:  //设置无图模式
+								if(settingInterface!=null)
+									settingInterface.fitlerPicLoading();
+								break;
+							case 6: //下载管理
+								if(settingInterface!=null)
+									settingInterface.manageDownload();
+								break;
+							case 7://退出系统
+								if(settingInterface!=null)
+									settingInterface.quit();
 
-						case 2://跳转到设置界面
-							if(settingInterface!=null)
-								settingInterface.browserSetting();
-							break;
-						case 3: // 跳转到历史记录界面
-							if(settingInterface!=null)
-								settingInterface.listHistory();
-							break;
-						case 4://分享
-							if(settingInterface!=null)
-								settingInterface.share();
-							break;
-						case 5:  //设置无图模式
-							if(settingInterface!=null)
-								settingInterface.fitlerPicLoading();
-							break;
-						case 6: //下载管理
-							if(settingInterface!=null)
-								settingInterface.manageDownload();
-							break;
-						case 7://退出系统
-							if(settingInterface!=null)
-								settingInterface.quit();
-
-							break;
-						default:
-							break;
+								break;
+							default:
+								break;
+							}
+							((BaseFragActivity)(SettingPagerFragment.this.getActivity())).removeFragment(SettingPagerFragment.this);
 						}
-						((BaseFragActivity)(SettingPagerFragment.this.getActivity())).removeFragment(SettingPagerFragment.this);
-					}
-				});
-			}
-			if(i==1){
-				lv.setOnItemClickListener(new OnItemClickListener(){
+					});
+				}
+				if(i==1){
+					appPage.setOnItemClickListener(new OnItemClickListener(){
 
-					@Override
-					public void onItemClick(AdapterView<?> parent,
-							View view, int position, long id) {
-						switch (position) {
-						case 0://页面翻转
-							if(settingInterface!=null)
-								settingInterface.pageTurningSwitch();
-							break;
-						case 1://网页无痕浏览模式
-							if(settingInterface!=null)
-								settingInterface.withoutTrace();
-							break;
-						case 2://网页全屏浏览模式
-							if(settingInterface!=null)
-								settingInterface.fullScreen();
-							break;
-						case 5://页面刷新
-							if(settingInterface!=null)
-								settingInterface.refresh();
-							break;
-						default:
-							break;
+						@Override
+						public void onItemClick(AdapterView<?> parent,
+								View view, int position, long id) {
+							switch (position) {
+							case 0://页面翻转
+								if(settingInterface!=null)
+									settingInterface.pageTurningSwitch();
+								break;
+							case 1://网页无痕浏览模式
+								if(settingInterface!=null)
+									settingInterface.withoutTrace();
+								break;
+							case 2://网页全屏浏览模式
+								if(settingInterface!=null)
+									settingInterface.fullScreen();
+								break;
+							case 5://页面刷新
+								if(settingInterface!=null)
+									settingInterface.refresh();
+								break;
+							default:
+								break;
+							}
+							((BaseFragActivity)(SettingPagerFragment.this.getActivity())).removeFragment(SettingPagerFragment.this);
 						}
-						((BaseFragActivity)(SettingPagerFragment.this.getActivity())).removeFragment(SettingPagerFragment.this);
-					}
-					
-				});
+						
+					});
+				}
 			}
-		}
-	}
-	public List<View> getPageViews() {
-		return mViewPages;
 	}
 	
 }
