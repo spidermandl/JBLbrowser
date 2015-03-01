2f1131d8715fcabbe540d21c348dc56575d9ac0e
xuhaiyan <haiyan.xu.vip@gmail.com>
2015-03-01 16:13:54 +0800

    settings fragment xml alreaday finashed

--- JBLBrowser/src/com/jbl/browser/fragment/MenuSettingFragment.java
+++ JBLBrowser/src/com/jbl/browser/fragment/MenuSettingFragment.java
@@ -1,40 +1,28 @@
 package com.jbl.browser.fragment;
 
-import android.app.AlertDialog;
-import android.app.AlertDialog.Builder;
-import android.content.DialogInterface;
 import android.os.Bundle;
 import android.view.LayoutInflater;
-import android.view.MotionEvent;
 import android.view.View;
 import android.view.ViewGroup;
-import android.view.Window;
-import android.webkit.WebSettings;
-import android.widget.AdapterView;
-import android.widget.AdapterView.OnItemClickListener;
 import android.widget.Button;
-import android.widget.ImageView;
-import android.widget.RelativeLayout;
-import android.widget.TextView;
 import android.widget.Toast;
 
 import com.actionbarsherlock.app.SherlockFragment;
-import com.jbl.browser.BrowserSettings;
 import com.jbl.browser.R;
-import com.jbl.browser.activity.BaseFragActivity;
-import com.jbl.browser.utils.BrightnessSettings;
-import com.jbl.browser.utils.JBLPreference;
+import com.jbl.browser.utils.SlipButton;
+import com.jbl.browser.utils.SlipButton.OnChangedListener;
 
 /**
  * 菜单设置选项fragment
  */
-public class MenuSettingFragment extends SherlockFragment implements OnItemClickListener{
+public class MenuSettingFragment extends SherlockFragment {
 	
 	public final static String TAG="MenuSettingFragment";
 	
 	//菜单设置选项内容 1 字体大小 2屏幕亮度 3默认浏览器 4 关于我们 5 清除数据 6恢复默认设置
-	//设置开关
-	private boolean mFlag=false;
+	private SlipButton mMenuSettingsSb;
+	private Button mMenuSetFont,mMenuSetIntensity,mMenuSetBrowser,mMenuSetAbout,mMenuSetClear,
+					mMentSetSetting;
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
 		// TODO Auto-generated method stub
@@ -45,9 +33,71 @@ public class MenuSettingFragment extends SherlockFragment implements OnItemClick
 	public View onCreateView(LayoutInflater inflater, ViewGroup container,
 			Bundle savedInstanceState) {
 		View view = inflater.inflate(R.layout.menuset_fragment, container, false);
+		mMenuSetFont=(Button)view.findViewById(R.id.menuset_but_font);
+		mMenuSetIntensity=(Button)view.findViewById(R.id.menuset_but_intensity);
+		mMenuSetAbout=(Button)view.findViewById(R.id.menuset_but_about);
+		mMenuSetClear=(Button)view.findViewById(R.id.menuset_but_clear);
+		mMentSetSetting=(Button)view.findViewById(R.id.menuset_but_settings);
+		mMenuSettingsSb=(SlipButton)view.findViewById(R.id.menuset_splitbutton);
+		mMenuSetBrowser=(Button)view.findViewById(R.id.menuset_but_browser);
+		//字体大小监听
+		mMenuSetFont.setOnClickListener(new View.OnClickListener() {
+			
+			@Override
+			public void onClick(View v) {
+				// TODO Auto-generated method stub
+				
+			}
+		});
+		//屏幕亮度监听
+		mMenuSetIntensity.setOnClickListener(new View.OnClickListener() {
+			
+			@Override
+			public void onClick(View v) {
+				// TODO Auto-generated method stub
+				Toast.makeText(getActivity(), "您选择了:1", 1).show();
+			}
+		});
+		//关于我们监听
+		mMenuSetAbout.setOnClickListener(new View.OnClickListener() {
+			
+			@Override
+			public void onClick(View v) {
+				// TODO Auto-generated method stub
+				Toast.makeText(getActivity(), "您选择了:2", 1).show();
+			}
+		});
+		//清除数据监听
+		mMenuSetClear.setOnClickListener(new View.OnClickListener() {
+			
+			@Override
+			public void onClick(View v) {
+				// TODO Auto-generated method stub
+				Toast.makeText(getActivity(), "您选择了:3", 1).show();
+			}
+		});
+		//恢复默认设置设置监听
+		mMentSetSetting.setOnClickListener(new View.OnClickListener() {
+			
+			@Override
+			public void onClick(View v) {
+				// TODO Auto-generated method stub
+				Toast.makeText(getActivity(), "您选择了:4", 1).show();
+			}
+		});
+		//监听设置默认浏览器滑动开关
+		mMenuSettingsSb.SetOnChangedListener(new OnChangedListener() {
+			
+			@Override
+			public void OnChanged(boolean CheckState) {
+				// TODO Auto-generated method stub
+				//mMenuSetBrowser.setText(CheckState ? "　默认浏览器打开" : "　默认浏览器关闭");
+				
+			}
+		});
 		return view;
 	}
-	@Override
+/*	@Override
 	public void onItemClick(AdapterView<?> parent, View view, int position,
 			long id) {
 		switch (position) {
@@ -77,8 +127,7 @@ public class MenuSettingFragment extends SherlockFragment implements OnItemClick
 				}
 			});
 			builder1.setNegativeButton("取消",new DialogInterface.OnClickListener() {
-				public void onClick(DialogInterface dialog, int which) {
-					
+				public void onClick(DialogInterface dialog, int which) {				
 				}
 			});
 			builder1.create().show();
@@ -155,5 +204,5 @@ public class MenuSettingFragment extends SherlockFragment implements OnItemClick
 		default:
 			return "锁定竖屏";
 		}
-	}
+	}*/
 }
