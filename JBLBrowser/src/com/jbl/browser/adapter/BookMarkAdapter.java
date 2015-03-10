d74928e2945a805b85596a01c46c163bb416e3c7
huyingying <1255793815@qq.com>
2015-03-10 21:11:14 +0800

    Add the collection of a plurality of delete

--- JBLBrowser/src/com/jbl/browser/adapter/BookMarkAdapter.java
+++ JBLBrowser/src/com/jbl/browser/adapter/BookMarkAdapter.java
@@ -5,23 +5,31 @@ import java.util.List;
 
 import com.jbl.browser.R;
 import com.jbl.browser.bean.BookMark;
+
+import android.annotation.SuppressLint;
 import android.content.Context;
+import android.graphics.drawable.Drawable;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
 import android.widget.ImageView;
+import android.widget.ListView;
 import android.widget.TextView;
 
 
+@SuppressLint("ResourceAsColor")
 public class BookMarkAdapter extends BaseAdapter{
 	List<BookMark> list_bookmark=new ArrayList<BookMark>();
 	 private LayoutInflater mInflater; 
 	 private Context mContext;
-	 public BookMarkAdapter(Context context, List<BookMark> list) {  
+	 private ListView listView;
+	 
+	 public BookMarkAdapter(Context context, List<BookMark> list,ListView listView) {  
 		    mContext=context;  
 	        mInflater = LayoutInflater.from(context);  
 	        list_bookmark= list;  
+	        this.listView=listView;
 	    } 
 	@Override
 	public int getCount() {
@@ -57,6 +65,7 @@ public class BookMarkAdapter extends BaseAdapter{
         }
         holder.urlName.setText(list_bookmark.get(position).getWebName());
         holder.urlAddress.setText(list_bookmark.get(position).getWebAddress());
+        updateBackground(position , convertView);
 		return convertView;
 	}
 	public class ViewHolder  
@@ -65,4 +74,17 @@ public class BookMarkAdapter extends BaseAdapter{
         public TextView urlName,urlAddress;  
         public ImageView image;
     }  
+	@SuppressLint({ "NewApi", "ResourceAsColor" })
+	public void updateBackground(int position, View view) {
+		int backgroundId=0;
+		if (listView.isItemChecked(position)) {
+			backgroundId = R.drawable.list_selected_holo_light;
+			Drawable background = mContext.getResources().getDrawable(backgroundId);
+			view.setBackgroundDrawable(background);
+		}else{
+			view.setBackgroundDrawable(null);
+		}
+		
+		
+	}
 }
