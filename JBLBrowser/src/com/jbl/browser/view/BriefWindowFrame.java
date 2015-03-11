e8beda44123e35e1963b184434a0ce825bb6a79e
Desmond Duan <Desmond@Desmonds-MacBook-Pro.local>
2015-03-12 01:35:09 +0800

    add delete func in multi pagers

---
+++ JBLBrowser/src/com/jbl/browser/view/BriefWindowFrame.java
@@ -0,0 +1,67 @@
+package com.jbl.browser.view;
+
+import com.jbl.browser.R;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.util.Log;
+import android.view.MotionEvent;
+import android.widget.ImageView;
+import android.widget.RelativeLayout;
+
+/**
+ * 装载多页缩略webview的viewgroup
+ * @author Desmond
+ *
+ */
+public class BriefWindowFrame extends RelativeLayout {
+
+	ImageView deleteWebView;
+	private Context ctx;
+	
+	public BriefWindowFrame(Context context) {
+		super(context);
+		init(context);
+	}
+	public BriefWindowFrame(Context context, AttributeSet attrs) {
+		super(context, attrs);
+		init(context);
+	}
+	public BriefWindowFrame(Context context, AttributeSet attrs, int defStyle) {
+		super(context, attrs, defStyle);
+		init(context);
+	}
+	
+	private void init(Context context){
+		ctx=context;
+
+	}
+	
+	@Override
+	protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
+		deleteWebView=(ImageView)this.findViewById(R.id.delete_webview);
+		super.onMeasure(widthMeasureSpec, heightMeasureSpec);
+	}
+	@Override
+	public boolean onInterceptTouchEvent(MotionEvent ev) {
+		float x=ev.getX();
+		float y=ev.getY();
+		Log.e("posision", "X: "+x+" Y: "+y+" WIDTH:"+this.getMeasuredWidth()+" HEIGHT;"+this.getMeasuredHeight()+" width:"+deleteWebView.getMeasuredWidth()+" height:"+deleteWebView.getMeasuredHeight());
+		if(deleteWebView!=null){
+			
+			if(x>=this.getMeasuredWidth()-deleteWebView.getMeasuredHeight()&&x<=this.getMeasuredWidth()
+					&&
+			   y>0&&y<deleteWebView.getMeasuredHeight()){
+				return super.onInterceptTouchEvent(ev);
+			}
+		}
+		return true;
+	}
+	
+	@Override
+	public boolean onTouchEvent(MotionEvent event) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+}
