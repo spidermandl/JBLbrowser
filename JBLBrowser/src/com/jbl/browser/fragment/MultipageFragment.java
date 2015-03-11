58fc8aa6d4efbabd6d200cc354b70b670c86c884
liumingran <1209725754@qq.com>
2015-03-11 11:23:17 +0800

    multipage

--- JBLBrowser/src/com/jbl/browser/fragment/MultipageFragment.java
+++ JBLBrowser/src/com/jbl/browser/fragment/MultipageFragment.java
@@ -1,6 +1,6 @@
 package com.jbl.browser.fragment;
 import java.util.ArrayList;
-
+import android.annotation.SuppressLint;
 import android.os.Bundle;
 import android.support.v4.view.ViewPager;
 import android.view.LayoutInflater;
@@ -8,13 +8,14 @@ import android.view.View;
 import android.view.View.OnClickListener;
 import android.view.ViewGroup;
 import android.widget.TextView;
-
 import com.actionbarsherlock.app.SherlockFragment;
 import com.jbl.browser.R;
 import com.jbl.browser.WebWindowManagement;
 import com.jbl.browser.activity.BaseFragActivity;
 import com.jbl.browser.adapter.MultipageAdapter;
 import com.jbl.browser.adapter.WebHorizontalViewAdapter;
+import com.jbl.browser.view.ScaleImageView;
+import com.jbl.browser.view.ScaleTextView;
 import com.jbl.browser.view.WebHorizontalView;
 import com.viewpager.indicator.CirclePageIndicator;
 
@@ -35,6 +36,7 @@ public class MultipageFragment extends SherlockFragment implements OnClickListen
 	private WebHorizontalViewAdapter mAdapter;
 	
 	private TextView newWindow;
+	private ScaleImageView multiPageNum;
 	
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
@@ -62,19 +64,25 @@ public class MultipageFragment extends SherlockFragment implements OnClickListen
         
         newWindow = (TextView)view.findViewById(R.id.new_window);
         newWindow.setOnClickListener(this);
+        multiPageNum=(ScaleImageView)view.findViewById(R.id.multi_page_num);
+        multiPageNum.setOnClickListener(this);
 		return view;
 	}
-
-//	@Override
-//	public void multipageWindow() {
-//		((BaseFragActivity)getActivity()).navigateTo(MainPageFragment.class,null,true, MainPageFragment.TAG);
-//		
-//	}
+	
 	@Override
 	public void onClick(View v) {
-		WebWindowManagement.getInstance().replaceWebViewWithIndex(
-				null, WebWindowManagement.getInstance().getCount(), true);
-		getFragmentManager().popBackStack();
+		switch (v.getId()) {
+		case R.id.new_window:
+			WebWindowManagement.getInstance().replaceWebViewWithIndex(
+					null, WebWindowManagement.getInstance().getCount(), true);
+			getFragmentManager().popBackStack();
+			break;
+		case R.id.multi_page_num:
+			getFragmentManager().popBackStack();
+		default:
+			break;
+		}
+		
 		
 	}
 
