c36fa9d5ab26dc6e2dd006c21a1f0eb37dc464a9
Desmond Duan <Desmond@Desmonds-MacBook-Pro.local>
2015-06-06 17:52:06 +0800

    remove unused code

--- JBLBrowser/src/com/jbl/browser/wifi/NormalWifiState.java
+++ JBLBrowser/src/com/jbl/browser/wifi/NormalWifiState.java
@@ -8,29 +8,6 @@ import com.jbl.browser.activity.WIFIService;
  *
  */
 public class NormalWifiState extends BaseState {
-
-	/**
-	 * 免费网络检测阶段
-	 */
-	public static enum AuthStage{
-		ISCMCC(0),//检测是否有cmcc
-		DATASTREAM(3),//数据流量
-		CONNECTED(4), //cmcc连接上
-		FAILED(5),//CMCC验证失败
-	    AUTHORITHED(6);//CMCC验证通过
-		// 定义私有变量
-		private int nCode;
-
-		// 构造函数，枚举类型只能为私有
-		private AuthStage(int _nCode) {
-			this.nCode = _nCode;
-		}
-
-		@Override
-		public String toString() {
-			return String.valueOf("wifi_status_type_"+this.nCode);
-		}
-	};
 	
 	public NormalWifiState(WIFIService m) {
 		super(m);
